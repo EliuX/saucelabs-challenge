fbe1da141992d836a1c9d1873a6947dfea6ab89f
EliuX <eliecerhdz@gmail.com>
2019-01-21 07:18:58 -0500

    ¨Firs commit

---
+++ src/main/java/com/saucelabs/magnificent/MagnificentApplication.java
@@ -0,0 +1,14 @@
+package com.saucelabs.magnificent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnificentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnificentApplication.class, args);
+	}
+
+}
+
